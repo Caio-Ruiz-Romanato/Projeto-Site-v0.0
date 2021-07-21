a4b3ad2720f4eb30cee9120ccf42e886755ea67f
Caio-Pixu <caioruizromanato@outlook.com>
2021-07-21 00:25:12 -0300

    Projeto-Site0

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
